f4136ced7c14b60dde47b4e2999aefc40d571924
talog <tee.talog@gmail.com>
2024-01-15 10:06:17 +0900

    read file

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
+import fs from "fs"
 import remarkParse from "remark-parse"
 import remarkStringify from "remark-stringify"
 import { unified } from "unified"
 
 const main = async () => {
-  const file = await unified()
+  const file = fs.readFileSync("tasks.md", "utf-8")
+
+  const tokens = await unified()
     .use(remarkParse)
     .use(remarkStringify)
-    .parse("# Hello, Neptune!")
+    .parse(file)
   // .process("# Hello, Neptune!")
 
-  console.log(file)
+  console.log(tokens)
 }
 
 main()
