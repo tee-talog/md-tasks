a121d52e28ca8c2393c59dd307d4865b4fe6cae5
talog <tee.talog@gmail.com>
2024-01-15 10:39:28 +0900

    search ast

--- index.ts
+++ index.ts
@@ -8,6 +8,18 @@ const main = async () => {
   const tokens = fromMarkdown(file)
   console.log(tokens)
 
+  const id = "1705225105518_0"
+
+  const list = tokens.children.find((e) => e.type === "list")
+  if (list.type === "list") {
+    const p = list.children[0].children[0]
+    if (p.type === "paragraph") {
+      if (p.children[0].type === "text") {
+        console.log(p.children[0].value)
+      }
+    }
+  }
+
   const text = toMarkdown(tokens)
   console.log(text)
 }
