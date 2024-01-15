fc2bc14248cf9cd0acbec35637cd10d9e1824f95
talog <tee.talog@gmail.com>
2024-01-15 10:19:24 +0900

    output markdown

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import fs from "fs"
 import remarkParse from "remark-parse"
-import remarkStringify from "remark-stringify"
+// import remarkStringify from "remark-stringify"
 import { unified } from "unified"
+import { toMarkdown } from "mdast-util-to-markdown"
 
 const main = async () => {
   const file = fs.readFileSync("tasks.md", "utf-8")
@@ -11,6 +12,9 @@ const main = async () => {
   // .process("# Hello, Neptune!")
 
   console.log(tokens)
+
+  const text = toMarkdown(tokens)
+  console.log(text)
 }
 
 main()
