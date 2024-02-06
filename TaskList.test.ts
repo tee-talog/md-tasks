2315c78a7f1c2932cf5a4e1f102b64ffa4e81aa1
talog <tee.talog@gmail.com>
2024-02-06 10:14:50 +0900

    markdown test

--- TaskList.test.ts
+++ TaskList.test.ts
@@ -1,7 +1,26 @@
-import { assertNotEquals } from "https://deno.land/std@0.214.0/assert/mod.ts"
+import {
+  assertNotEquals,
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@0.214.0/assert/mod.ts"
+import { describe, it } from "https://deno.land/std@0.214.0/testing/bdd.ts"
+
+import { fromMarkdown } from "mdast-util-from-markdown"
+
 import TaskList from "./TaskList.ts"
 
-Deno.test("first test", () => {
-  const taskList = new TaskList({ type: "root", children: [] })
-  assertNotEquals(taskList, null)
+const token = (strs: TemplateStringsArray, ...args: string[]) => {
+  const res: string[] = []
+  for (let i = 0; i < strs.length; i++) {
+    const s = (strs[i] ?? "") + (args[i] ?? "")
+    res.push(s)
+  }
+  return fromMarkdown(res.join(""))
+}
+
+describe("addItem", () => {
+  it("セクションがないとき、エラーをスローすること", () => {
+    const taskList = new TaskList(token``)
+    assertThrows(() => taskList.addItem("a", "text 1"))
+  })
 })
